0cde848d9cb38434295e9d5350ee589f579afbe9
alvesmog <alvesmog@gmail.com>
2019-10-12 03:02:34 -0300

    ...

--- src/modulo3_exercicio_uml/RepositorioContas.java
+++ src/modulo3_exercicio_uml/RepositorioContas.java
@@ -1,5 +1,17 @@
 package modulo3_exercicio_uml;
 
 public interface RepositorioContas {
+	
+	public default void inserir(Conta conta) {
+		
+	}
+	
+	public default Conta procurar(String numeroConta) {
+		
+	}
+	
+	public default void remover(String numeroConta) {
+		
+	}
 
 }
--- src/modulo3_exercicio_uml/RepositorioContasArray.java
+++ src/modulo3_exercicio_uml/RepositorioContasArray.java
@@ -1,5 +1,36 @@
 package modulo3_exercicio_uml;
 
-public class RepositorioContasArray {
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class RepositorioContasArray implements RepositorioContas {
+	
+	private static Arrays contas;
+	private static int indice;
+	
+	
+
+	public RepositorioContasArray(int tamanho){
+		
+	}
+	
+	@Override
+	public void inserir(Conta conta) {
+		
+		
+		
+	}
+	
+	@Override
+	public Conta procurar(String numeroConta) {
+		
+	}
+	
+	@Override
+	public void remover(String numeroConta) {
+		
+	}
+	
+	
 
 }
